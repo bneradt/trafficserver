7d1ffc160949a70ca05f1e12fd592ff95b6247a8
Brian Neradt <brian.neradt@gmail.com>
2020-10-28 14:32:43 -0500

    Make double Au test more reliable.
    In my test environment, the double test fails at least 1% of the time.  I changed an Au test ready condition
    to polling in a bash while loop.  I ran the changed test 500 times, and there were 0 failures.  I also
    eliminated the "done" temp file, which the current version of the test writes in whatever directory
    autest was run from (and doesn't delete it at the end of the test).
    
    (cherry picked from commit 55482d4c8d5abd13968377783bbfc7930c8bc43f)

--- tests/gold_tests/continuations/double.test.py
+++ tests/gold_tests/continuations/double.test.py
@@ -41,8 +41,6 @@ server.addResponse("sessionfile.log", request_header, response_header)
 ts.Disk.records_config.update({
     'proxy.config.diags.debug.enabled': 1,
     'proxy.config.diags.debug.tags': 'continuations_verify.*',
-    'proxy.config.http.cache.http' : 0, #disable cache to simply the test.
-    'proxy.config.cache.enable_read_while_writer' : 0
 })
 ts.Disk.remap_config.AddLine(
     'map http://double.test:{0} http://127.0.0.1:{1}'.format(ts.Variables.port, server.Variables.Port)
@@ -66,6 +64,7 @@ ts.StartAfter(*ps)
 server.StartAfter(*ps)
 tr.StillRunningAfter = ts
 
+<<<<<<< HEAD
 comparator_command = '''
 if test "`traffic_ctl metric get continuations_verify.{0}.close.1 | cut -d ' ' -f 2`" -eq "`traffic_ctl metric get continuations_verify.{0}.close.2 | cut -d ' ' -f 2`" ; then\
      echo yes;\
@@ -76,14 +75,69 @@ if test "`traffic_ctl metric get continuations_verify.{0}.close.1 | cut -d ' ' -
 
 records = ts.Disk.File(os.path.join(ts.Variables.RUNTIMEDIR, "records.snap"))
 
+||||||| parent of 55482d4c8... Make double Au test more reliable. (#7294)
+# Signal that all the curl processes have completed
+tr = Test.AddTestRun("Curl Done")
+tr.Processes.Default.Command = "traffic_ctl plugin msg done done"
+tr.Processes.Default.ReturnCode = 0
+tr.Processes.Default.Env = ts.Env
+tr.StillRunningAfter = ts
+
+# Parking this as a ready tester on a meaningless process
+# To stall the test runs that check for the stats until the
+# stats have propagated and are ready to read.
+
+
+def make_done_stat_ready(tsenv):
+    def done_stat_ready(process, hasRunFor, **kw):
+        retval = subprocess.run("traffic_ctl metric get continuations_verify.test.done > done  2> /dev/null", shell=True, env=tsenv)
+        if retval.returncode == 0:
+            retval = subprocess.run("grep 1 done > /dev/null", shell=True, env=tsenv)
+        return retval.returncode == 0
+
+    return done_stat_ready
+
+
+=======
+# Signal that all the curl processes have completed and poll for done metric
+tr = Test.AddTestRun("Curl Done")
+tr.Processes.Default.Command = (
+    "traffic_ctl plugin msg done done ; "
+    "N=60 ; "
+    "while (( N > 0 )) ; "
+    "do "
+    "sleep 1 ; "
+    'if [[ "$$( traffic_ctl metric get continuations_verify.test.done )" = '
+    '"continuations_verify.test.done 1" ]] ; then exit 0 ; '
+    "fi ; "
+    "let N=N-1 ; "
+    "done ; "
+    "echo TIMEOUT ; "
+    "exit 1"
+)
+tr.Processes.Default.ReturnCode = 0
+tr.Processes.Default.Env = ts.Env
+tr.StillRunningAfter = ts
+
+>>>>>>> 55482d4c8... Make double Au test more reliable. (#7294)
 # number of sessions/transactions opened and closed are equal
+<<<<<<< HEAD
 tr = Test.AddTestRun()
 tr.DelayStart = 10 # wait for stats to be updated
+||||||| parent of 55482d4c8... Make double Au test more reliable. (#7294)
+tr = Test.AddTestRun("Check Ssn")
+server2.StartupTimeout = 60
+# Again, here the imporant thing is the ready function not the server2 process
+tr.Processes.Default.StartBefore(server2, ready=make_done_stat_ready(ts.Env))
+=======
+tr = Test.AddTestRun("Check Ssn")
+>>>>>>> 55482d4c8... Make double Au test more reliable. (#7294)
 tr.Processes.Default.Command = comparator_command.format('ssn')
 tr.Processes.Default.ReturnCode = 0
 tr.Processes.Default.Env = ts.Env
 tr.Processes.Default.Streams.stdout = Testers.ContainsExpression("yes", 'should verify contents')
 tr.StillRunningAfter = ts
+<<<<<<< HEAD
 # for debugging session number
 ssn1 = tr.Processes.Process("session1", 'traffic_ctl metric get continuations_verify.ssn.close.1 > ssn1')
 ssn2 = tr.Processes.Process("session2", 'traffic_ctl metric get continuations_verify.ssn.close.2 > ssn2')
@@ -91,6 +145,10 @@ ssn1.Env = ts.Env
 ssn2.Env = ts.Env
 tr.Processes.Default.StartBefore(ssn1)
 tr.Processes.Default.StartBefore(ssn2)
+||||||| parent of 55482d4c8... Make double Au test more reliable. (#7294)
+tr.StillRunningAfter = server2
+=======
+>>>>>>> 55482d4c8... Make double Au test more reliable. (#7294)
 
 tr = Test.AddTestRun()
 tr.DelayStart = 10 # wait for stats to be updated
@@ -99,6 +157,7 @@ tr.Processes.Default.ReturnCode = 0
 tr.Processes.Default.Env = ts.Env
 tr.Processes.Default.Streams.stdout = Testers.ContainsExpression("yes", 'should verify contents')
 tr.StillRunningAfter = ts
+<<<<<<< HEAD
 # for debugging transaction number
 txn1 = tr.Processes.Process("transaction1", 'traffic_ctl metric get continuations_verify.txn.close.1 > txn1')
 txn2 = tr.Processes.Process("transaction2", 'traffic_ctl metric get continuations_verify.txn.close.2 > txn2')
@@ -125,3 +184,7 @@ tr.Processes.Default.Env = ts.Env
 tr.Processes.Default.Streams.stdout = Testers.ContainsExpression(
     "continuations_verify.txn.close.1 {}".format(numberOfRequests), 'should be the number of transactions we made')
 tr.StillRunningAfter = ts
+||||||| parent of 55482d4c8... Make double Au test more reliable. (#7294)
+tr.StillRunningAfter = server2
+=======
+>>>>>>> 55482d4c8... Make double Au test more reliable. (#7294)
