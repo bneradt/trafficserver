ce7c6c694ec309cfe6a0fd6e770513400df5dd76
Brian Neradt <brian.neradt@yahooinc.com>
2025-06-10 13:24:21 -0500

    SnowflakeID for organizationally unique identifiers (#1066)
    UUID values are 128 bit values designed to be univerally unique
    identifiers across all practical uses and environments. Snowflake ID
    values are 64 bit values that are designed with a more limited goal: to
    be unique across all values in a single organization. By limiting their
    scope, they limit their size which is helpful for performance and
    storage needs.
    
    This updates the connection id to use SnowflakeID values so that they
    are unique across reboots and typically unique across ATS instances in a
    cdn.
    
    This also adds psfid, a logging field similar to puuid, but that logs
    the process's snowflake ID instead of its UUID.

--- src/traffic_server/traffic_server.cc
+++ src/traffic_server/traffic_server.cc
@@ -2055,7 +2055,7 @@ main(int /* argc ATS_UNUSED */, const char **argv)
   Machine::init(hostname, &machine_addr.sa);
   ats_free(hostname);
 
-  RecRegisterStatString(RECT_PROCESS, "proxy.process.version.server.uuid", (char *)Machine::instance()->uuid.getString(),
+  RecRegisterStatString(RECT_PROCESS, "proxy.process.version.server.uuid", (char *)Machine::instance()->process_uuid.getString(),
                         RECP_NON_PERSISTENT);
 
   REC_ReadConfigInteger(res_track_memory, "proxy.config.res_track_memory");
